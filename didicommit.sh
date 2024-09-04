8d6865a071b6fd26bd96f01ef37075e489effba1
Kelvin Shadewing <kelvin@shadewing.net>
2024-09-04 00:14:54 -0400

    Improve didicommit.sh

--- didicommit.sh
+++ didicommit.sh
@@ -2,22 +2,20 @@
 # any git repos with uncommitted files exist.
 
 function didicommit() {
-	RED='\033[1;31m'
-	NC='\033[0m'
+	local RED='\033[1;31m'
+	local NC='\033[0m'
 
 	for fn in `find /home -name ".git"`; do
 		if [ -d "$fn" ]; then
 			cd "$fn"
-		else
-			continue 1
-		fi
-		cd ..
-		A=`git status --porcelain`
-		B=`echo "$A" | sed 1d | wc -l`
-		B+=" files changed"
-		if [[ $B != 0* ]]; then
-			pwd
-			printf "${RED}$B${NC}\n"
+			cd ..
+			local A=$(git status --porcelain)
+			local B=$(echo "$A" | sed 1d | wc -l)
+			B+=" files changed"
+			if [[ $B != 0* ]]; then
+				pwd
+				printf "${RED}$B${NC}\n\n"
+			fi
 		fi
 	done
 }
